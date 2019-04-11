27373761d46da782421de89153bde23c008e3020
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-11 13:41:41 -0700

    working tabs, trying to sort thru AM lesson

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -27,4 +27,3 @@ class Dropdown {
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
 
-// menu.classList.toggle('menu--open');
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,12 +1,12 @@
 
 class TabLink {
-  constructor(element) {
+  constructor(tabLink) {
     // Assign this.element to the passed in DOM element
-    // this.element;
+    this.element = tabLink;
     
     // Get the custom data attribute on the Link
-    // this.data;
-    
+    this.data = document.querySelector(`.tabs-link[data-tab="${this.element.dataset.tab}"]`);
+    console.log(this.data);
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
     
@@ -59,4 +59,6 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+links = document.querySelectorAll('.tabs-link');
+console.log(links);
+links.forEach(link => new TabLink(link));
